a9ec3a8959100dce0fc12a11bc8d0aaab601013b
Ignacio121990 <steel_man905@hotmail.com>
2016-03-04 14:24:32 -0600

    Update Herencia.js

--- rv/Herencia.js
+++ rv/Herencia.js
@@ -10,7 +10,7 @@ this.add(this.pierna);
 this.add(this.pie);
 }
 
-Pierna.prototype= new THREE.Objetc3D;
+Pierna.prototype= new THREE.Object3D;
 function setup(){
   var cuerpo= new THREE.Mesh(new THREE.CylinderGeometry(1,2,5,10));
   piernaD=new Pierna();
