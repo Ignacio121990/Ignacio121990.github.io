516f977bc33a766d160736d2b767ecb0ea4f7ae5
Ignacio121990 <steel_man905@hotmail.com>
2016-05-18 10:40:47 -0500

    Update kirbyperspective.js

--- rv/kirbyperspective.js
+++ rv/kirbyperspective.js
@@ -43,17 +43,15 @@ function setup(){
 
 escena=new THREE.Scene();
 escena.add(malla);
-
 camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
 
-//5 ancho 8 altura
+
 camara2 = new THREE.OrthographicCamera( 8 / - 2, 8 / 2, 5 / 2, 5 / - 2, 1, 1000 );
 camara2.position.z=5;
 camara2.position.x=1;
 
-//5 sobre 8 es ancho contra altura
-//camara3 = new THREE.PerspectiveCamera( 45, 5 / 8, 1, 1000 );
+
 camara3 = new THREE.PerspectiveCamera( 30, 5 / 8, 1, 1000 );
 camara3.position.z=10;
 
@@ -65,15 +63,9 @@ renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
 
-mallat=new THREE.Mesh(formac);
-mallat.position.z=1;
-mallat.rotation.z=0.75;
-mallat.rotation.x=-0.4;
-mallat.scale.set( 0.1, 0.1, 0.1 );
 
 escena=new THREE.Scene();
 escena.add(malla);
-escena.add(mallat);
 camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
 
@@ -83,7 +75,7 @@ camara2.position.z=5;
 camara2.position.x=1;
 
 //5 sobre 8 es ancho contra altura
-//camara3 = new THREE.PerspectiveCamera( 45, 5 / 8, 1, 1000 );
+
 camara3 = new THREE.PerspectiveCamera( 30, 5 / 8, 1, 1000 );
 camara3.position.z=10;
 
@@ -112,6 +104,6 @@ requestAnimationFrame(loop);
 
 }
 
-var camara,camara2,camara3,escena,renderer,malla,mallat;
+var camara,camara2,camara3,escena,renderer,malla;
 setup();
 loop();
