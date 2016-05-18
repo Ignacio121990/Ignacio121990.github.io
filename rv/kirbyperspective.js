76d213ee65be23536cb255384b1e70d1bed34191
Ignacio121990 <steel_man905@hotmail.com>
2016-05-18 11:11:31 -0500

    Update kirbyperspective.js

--- rv/kirbyperspective.js
+++ rv/kirbyperspective.js
@@ -36,12 +36,12 @@ function setup(){
   escena = new THREE.Scene();
   escena.add(malla);
   camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=20;
 
 //5 ancho 8 altura
 camara2 = new THREE.OrthographicCamera( 8 / - 2, 8 / 2, 5 / 2, 5 / - 2, 1, 1000 );
-camara2.position.z=5;
-camara2.position.x=1;
+camara2.position.z=20;
+camara2.position.x=10;
 
 //5 sobre 8 es ancho contra altura
 //camara3 = new THREE.PerspectiveCamera( 45, 5 / 8, 1, 1000 );
