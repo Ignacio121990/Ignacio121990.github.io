27840cf36ab66a6eb12b054d071efacd8f06cf47
Ignacio121990 <steel_man905@hotmail.com>
2016-05-18 11:01:35 -0500

    Update kirbyperspective.js

--- rv/kirbyperspective.js
+++ rv/kirbyperspective.js
@@ -1,48 +1,39 @@
-function Completo(){
-  THREE.Object3D.call(this);
-  THREE.ImageUtils.crossOrigin = '';
-  var cara = THREE.ImageUtils.loadTexture('http://Ignacio121990.github.io/12899665_10153684992446843_1267488120_n.jpg');
-  var brazos = THREE.ImageUtils.loadTexture('http://Ignacio121990.github.io/12899889_10153685013876843_569751267_n.jpg');
-  var pies = THREE.ImageUtils.loadTexture('http://Ignacio121990.github.io/12919388_10153685013881843_2062601905_o.jpg');  
+function setup(){
+  var esferaForma = new THREE.SphereGeometry(3);
+  var cilindroForma = new THREE.CylinderGeometry(0.5,0.5,4);
+  var rectanguloForma = new THREE.BoxGeometry(0.5,1,2);
+  
+  var cuerpo = new THREE.Mesh(esferaForma);
+  var brazoI = new THREE.Mesh(cilindroForma);
+  var brazoD = new THREE.Mesh(cilindroForma);
+  var pieI = new THREE.Mesh(rectanguloForma);
+  var pieD = new THREE.Mesh(rectanguloForma);
+  
+  brazoI.position.x=2.5;
+  brazoD.position.x=-2.5;
+  pieD.position.y=-3;
+  pieD.position.x=1;
+  pieI.position.y=-3;
+  pieI.position.x=-1;
+  pieD.position.z=1;
+  pieI.position.z=1;
+  brazoD.rotation.z=1.5;
+  brazoI.rotation.z=-1.5;
   
   
-   
-  this.cuerpo = new THREE.Mesh(new THREE.SphereGeometry(3,100,100),new THREE.MeshLambertMaterial({map:cara}));
-  this.brazoI = new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,3),new THREE.MeshLambertMaterial({map:brazos}));
-  this.brazoD = new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,3),new THREE.MeshLambertMaterial({map:brazos}));
-  this.pieI = new THREE.Mesh(new THREE.BoxGeometry(2,1,0.5),new THREE.MeshLambertMaterial({map:pies}));
-  this.pieD = new THREE.Mesh(new THREE.BoxGeometry(2,1,0.5),new THREE.MeshLambertMaterial({map:pies}));
+  var forma = new THREE.Geometry();
   
-  this.brazoI.position.z=2.3;
-  this.brazoD.position.z=-2.3;
-  this.pieD.position.y=-3;
-  this.pieD.position.z=1;
-  this.pieI.position.y=-3;
-  this.pieI.position.z=-1;
-  this.pieD.position.x=1;
-  this.pieI.position.x=1;
-  this.brazoD.rotation.x=1.5;
-  this.brazoI.rotation.x=-1.5;
+  THREE.GeometryUtils.merge( forma, cuerpo);
+  THREE.GeometryUtils.merge( forma, brazoI);
+  THREE.GeometryUtils.merge( forma, brazoD);
+  THREE.GeometryUtils.merge( forma, pieI);
+  THREE.GeometryUtils.merge( forma, pieD);
   
- 
-
-Completo.prototype = new THREE.Object3D();
- kirby = new Completo();
-  step  =0.01;
-  stepbrazo = 0.017;
-  
-  luzPuntual = new THREE.PointLight(0xffffff);
-  luzPuntual.position.x=30;
-  luzPuntual.position.y=30;
-  luzPuntual.position.z=30;
+  malla = new THREE.Mesh(forma);
   
   escena = new THREE.Scene();
-  escena.add(kirby);
-  escena.add(luzPuntual);
-    escena=new THREE.Scene();
-escena.add(malla);
-escena.add(mallat);
-camara=new THREE.PerspectiveCamera();
+  escena.add(malla);
+  camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 //5 ancho 8 altura
@@ -62,28 +53,24 @@ escena.add(camara3);
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
-
 }
+
+
+
 function loop(){
-requestAnimationFrame( loop );
-renderer.render (escena, camara);
-if (Math.abs(kirby.pieD.rotation.z) > .3 )
-  step = -step;
 
-if (Math.abs(kirby.brazoD.rotation.x) > 2 || Math.abs(kirby.brazoD.rotation.x) < 1)
-  stepbrazo = -stepbrazo;
 
-kirby.brazoD.rotation.x += stepbrazo;
-kirby.brazoI.rotation.x += stepbrazo;
-kirby.pieD.rotation.z += step;
-kirby.pieI.rotation.z -= step;
+if (keyboard.pressed("P")) {
+renderer.render(escena,camara3);
+}
+else
+{
+renderer.render(escena,camara2);
+}
+requestAnimationFrame(loop);
 
-//kirby.rotation.x += 0.01;
-kirby.rotation.y += 0.01;
 }
 
-var escena, camara, renderer;
-var step, stepbrazo;
-
-Completo();
+var camara,camara2,camara3,escena,renderer,malla,mallat;
+setup();
 loop();
