2a1c02b377c1bf5d298de594980282a0d239d2cb
Ignacio121990 <steel_man905@hotmail.com>
2016-05-21 23:43:19 -0500

    Update robotpared.js

--- rv/robotpared.js
+++ rv/robotpared.js
@@ -25,7 +25,7 @@ function kirby(x=0, y=0){
   this.add(this.brazoD)
   this.add(this.cuerpo)
  
-   this.luzr=new THREE.SpotLight(0xffffff,4,0.5,.03);
+   this.luzr=new THREE.SpotLight(0xffffff,4,2000,.01);
  this.luzr.target.updateMatrixWorld();
  this.luzr.target.position.set(10,0,0);
  this.add(this.luzr);
