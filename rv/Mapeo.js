2a9fdc6127fdf976b7769d83fea1071052e088fd
Ignacio121990 <steel_man905@hotmail.com>
2016-05-28 14:13:24 -0500

    Update Mapeo.js

--- rv/Mapeo.js
+++ rv/Mapeo.js
@@ -73,7 +73,7 @@ Environment.prototype.setMap=function(map){
 kirby.prototype.sense=function(environment){
  this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
  var obstaculo = this.sensor.intersectObjects(environment.children,true);
- 
+}
 
 kirby.prototype.plan = function(environment){
  this.actuator.commands=[];
